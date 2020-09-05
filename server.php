50e62a9b5dfeae4400d81adc9a69e04084867bc8
lavanya5g5 <70144300+lavanya5g5@users.noreply.github.com>
2020-09-05 11:31:07 +0530

    Add files via upload

---
+++ server.php
@@ -0,0 +1,14 @@
+<?php
+  $first=$_POST['num1'];
+  $second=$_POST['num2'];
+  $sum=$first+$second;
+  $diff=$first-$second;
+ $mul=$first*$second;
+ $div=$first/$second;
+ $mod=$first%$second;
+   echo " sum of two numbers is : $sum"."<br>";
+ echo " diff of two numbers is : $diff"."<br>";
+echo " mul of two numbers is : $mul"."<br>";
+echo " div of two numbers is : $div"."<br>";
+echo " mod of two numbers is : $mod"."<br>";
+?>
